638cfa527756c9b284cb1c031f9676c621a098dd
fgnpl <120001795+fgnpl@users.noreply.github.com>
2022-12-16 21:59:20 +0300

    Update virtual-calculator.py
    калькулятор обрабатывает больше ситуаций

--- virtual-calculator.py
+++ virtual-calculator.py
@@ -27,34 +27,35 @@ class Button:
     def draw(self, image):
         # Рисуем контур прямоугольника
         cv2.rectangle(image, self.position, (self.position[0] + self.width, self.position[1] + self.height),
-                      (50, 50, 50), 3)
+                      (25, 25, 25), 3)
         # Заполняем цветом
         cv2.rectangle(image, self.position, (self.position[0] + self.width, self.position[1] + self.height),
-                      (199, 199, 199), cv2.FILLED)
+                      (255, 248, 240), cv2.FILLED)
         # Рисуем текст
         cv2.putText(image, self.text, (self.position[0] + 38, self.position[1] + 58), cv2.FONT_HERSHEY_PLAIN, 2,
-                    (50, 50, 50), 2)
+                    (25, 25, 25), 2)
 
 
 # Обычно камера имеет номер 0
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(3, 1920)
+cap.set(4, 1080)
 
 # Кнопки
 buttonsText = [['7', '4', '1', '0'],
-               ['8', '5', '2', '/'],
-               ['9', '6', '3', '.'],
+               ['8', '5', '2', '.'],
+               ['9', '6', '3', '/'],
                ['*', '-', '+', '=']]
+special = ["*", ".", "/", "+", "-"]
 buttons = []
 for i in range(4):
     for j in range(4):
         x = i * 100 + 800
-        y = j * 100 + 150
+        y = j * 100 + 200
         buttons.append(Button(100, 100, (x, y), buttonsText[i][j]))
 
 # Операции и переменные калькулятора
-op = ""
+op = "12"
 
 # Создаем детектор
 handsDetector = mp.solutions.hands.Hands(min_detection_confidence=0.87, max_num_hands=1)
@@ -66,23 +67,23 @@ while cap.isOpened():
     # Переводим его в формат RGB для распознавания
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Отражаем горизонтально, переводим в BGR и показываем результат
+    # Отражаем горизонтально
     res_img = cv2.cvtColor(np.fliplr(img), cv2.COLOR_RGB2BGR)
 
     # Создание экрана калькулятора
-    cv2.rectangle(res_img, (800, 50), (1200, 170), (50, 50, 50), 3)
-    cv2.rectangle(res_img, (800, 50), (1200, 170), (199, 199, 199), cv2.FILLED)
+    cv2.rectangle(res_img, (800, 100), (1200, 200), (50, 50, 50), 3)
+    cv2.rectangle(res_img, (800, 100), (1200, 200), (240, 248, 255), cv2.FILLED)
 
     # Рисование кнопок
     for button in buttons:
         button.draw(res_img)
 
     # Распознаем руку
-    results = handsDetector.process(img,)
+    results = handsDetector.process(res_img,)
 
     # Рисуем распознанное, если распозналось
     if results.multi_hand_landmarks:
-        mp.solutions.drawing_utils.draw_landmarks(img,
+        mp.solutions.drawing_utils.draw_landmarks(res_img,
                                                   results.multi_hand_landmarks[0], mp.solutions.hands.HAND_CONNECTIONS)
 
         # Координаты указательного пальца
@@ -91,6 +92,7 @@ while cap.isOpened():
         # Координаты среднего пальца
         x12 = results.multi_hand_landmarks[0].landmark[12].x * img.shape[0]
         y12 = results.multi_hand_landmarks[0].landmark[12].y * img.shape[1]
+
         # Расстояние между ними
         distance = int(np.hypot((x12 - x8), (y12 - y8)))
         if distance < 30:
@@ -101,12 +103,18 @@ while cap.isOpened():
                     value = ret[1]
                     # Считаем результат операций
                     if value == '=':
-                        op = str(eval(op))
+                        if op[-2:] == ["/", "0"]:
+                            op = "Error"
+                        else:
+                            op = str(eval(op))
+                    # Исключение повторов
+                    elif value in special and op[-1] == value:
+                        pass
                     else:
                         op += value
 
     # Вывод результата калькулятора
-    cv2.putText(res_img, op, (810, 110), cv2.FONT_HERSHEY_PLAIN, 3, (50, 50, 50), 3)
+    cv2.putText(res_img, op, (810, 165), cv2.FONT_HERSHEY_PLAIN, 3, (50, 50, 50), 3)
 
     # Задерживаем на 1 миллисекунду, ждем нажатия q
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
@@ -116,4 +124,3 @@ while cap.isOpened():
     cv2.imshow("camera", res_img)
 
 # Освобождаем ресурсы
-handsDetector.close()
