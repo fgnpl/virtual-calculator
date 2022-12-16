158b825a229961c9ab3e3ee1f4378a0c090a051c
fgnpl <120001795+fgnpl@users.noreply.github.com>
2022-12-16 22:01:09 +0300

    Update virtual-calculator.py
    добавлены кнопки стирания и удаления последнего элемента на калькуляторе, отформатирован вывод калькулятора, исправлена проблема с распознаванием нажатия кнопок

--- virtual-calculator.py
+++ virtual-calculator.py
@@ -12,15 +12,9 @@ class Button:
 
     def clicked(self, x, y):
         # Попала ли точка в поле кнопки по оси х
-        print(self.position[0], x, self.position[0] + self.width)
         if self.position[0] < x < self.position[0] + self.width:
-            print("x")
             # Попала ли точка в поле кнопки по оси y
             if self.position[1] < y < self.position[1] + self.height:
-                print("y")
-                # Подсвечиваем кнопку
-                cv2.rectangle(img, self.position, (self.position[0] + self.width, self.position[1] + self.height),
-                              (255, 255, 255), 3)
                 return True, self.text
         return False, 0
 
@@ -28,24 +22,26 @@ class Button:
         # Рисуем контур прямоугольника
         cv2.rectangle(image, self.position, (self.position[0] + self.width, self.position[1] + self.height),
                       (25, 25, 25), 3)
-        # Заполняем цветом
-        cv2.rectangle(image, self.position, (self.position[0] + self.width, self.position[1] + self.height),
-                      (255, 248, 240), cv2.FILLED)
+
         # Рисуем текст
-        cv2.putText(image, self.text, (self.position[0] + 38, self.position[1] + 58), cv2.FONT_HERSHEY_PLAIN, 2,
-                    (25, 25, 25), 2)
+        if self.text == "AC" or self.text == "DEL":
+            cv2.putText(image, self.text, (self.position[0] + 75, self.position[1] + 35), cv2.FONT_HERSHEY_PLAIN, 2,
+                        (25, 25, 25), 2)
+        else:
+            cv2.putText(image, self.text, (self.position[0] + 38, self.position[1] + 58), cv2.FONT_HERSHEY_PLAIN, 2,
+                        (25, 25, 25), 2)
 
 
 # Обычно камера имеет номер 0
 cap = cv2.VideoCapture(0)
-cap.set(3, 1920)
-cap.set(4, 1080)
+cap.set(3, 1280)
+cap.set(4, 720)
 
 # Кнопки
-buttonsText = [['7', '4', '1', '0'],
+buttonsText = [['7', '4', '1', '='],
                ['8', '5', '2', '.'],
                ['9', '6', '3', '/'],
-               ['*', '-', '+', '=']]
+               ['*', '-', '+', '0']]
 special = ["*", ".", "/", "+", "-"]
 buttons = []
 for i in range(4):
@@ -54,16 +50,26 @@ for i in range(4):
         y = j * 100 + 200
         buttons.append(Button(100, 100, (x, y), buttonsText[i][j]))
 
-# Операции и переменные калькулятора
-op = "12"
+# Кнопка clear
+x = 800
+y = 600
+buttons.append(Button(200, 50, (x, y), "AC"))
+
+# Кнопка delete
+x = 1000
+y = 600
+buttons.append(Button(200, 50, (x, y), "DEL"))
+
+# Переменные
+op = ""
+delay = 0
 
 # Создаем детектор
-handsDetector = mp.solutions.hands.Hands(min_detection_confidence=0.87, max_num_hands=1)
+handsDetector = mp.solutions.hands.Hands(min_detection_confidence=0.1, max_num_hands=1)
 
 while cap.isOpened():
     # Читаем очередной кадр
     ret, img = cap.read()
-
     # Переводим его в формат RGB для распознавания
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -71,8 +77,8 @@ while cap.isOpened():
     res_img = cv2.cvtColor(np.fliplr(img), cv2.COLOR_RGB2BGR)
 
     # Создание экрана калькулятора
-    cv2.rectangle(res_img, (800, 100), (1200, 200), (50, 50, 50), 3)
-    cv2.rectangle(res_img, (800, 100), (1200, 200), (240, 248, 255), cv2.FILLED)
+    cv2.rectangle(res_img, (800, 100), (1200, 200), (25, 25, 25), 3)
+    # cv2.rectangle(res_img, (800, 100), (1200, 200), (240, 248, 255), cv2.FILLED)
 
     # Рисование кнопок
     for button in buttons:
@@ -87,34 +93,58 @@ while cap.isOpened():
                                                   results.multi_hand_landmarks[0], mp.solutions.hands.HAND_CONNECTIONS)
 
         # Координаты указательного пальца
-        x8 = results.multi_hand_landmarks[0].landmark[8].x * img.shape[0]
-        y8 = results.multi_hand_landmarks[0].landmark[8].y * img.shape[1]
+        x8 = results.multi_hand_landmarks[0].landmark[8].x * img.shape[1]
+        y8 = results.multi_hand_landmarks[0].landmark[8].y * img.shape[0]
         # Координаты среднего пальца
-        x12 = results.multi_hand_landmarks[0].landmark[12].x * img.shape[0]
-        y12 = results.multi_hand_landmarks[0].landmark[12].y * img.shape[1]
+        x12 = results.multi_hand_landmarks[0].landmark[12].x * img.shape[1]
+        y12 = results.multi_hand_landmarks[0].landmark[12].y * img.shape[0]
 
         # Расстояние между ними
         distance = int(np.hypot((x12 - x8), (y12 - y8)))
-        if distance < 30:
+        if distance < 43:
             # Проверяем нажата ли кнопка и какая
             for button in buttons:
                 ret = button.clicked(x8, y8)
-                if ret[0]:
+                if ret[0] and delay == 0:
+                    delay = 1
                     value = ret[1]
-                    # Считаем результат операций
+                    # Обрабатываем операции
                     if value == '=':
-                        if op[-2:] == ["/", "0"]:
-                            op = "Error"
+                        if op[-1] in special:
+                            pass
                         else:
-                            op = str(eval(op))
+                            if op[-2:] == ["/", "0"]:
+                                op = "Error"
+                            else:
+                                res = eval(op)
+                                if len(str(res)) > 12:
+                                    # Форматированрие строки
+                                    sn = "{:.2e}".format(res)
+                                    n = len(sn[sn.index("e") + 1:])
+                                    rounded = round(res, (9 - n))
+                                    res = "{:e}".format(res)
+                                op = str(res)
+                    elif value == "AC":
+                        op = ""
+                    elif value == "DEL":
+                        op = op[:-1]
                     # Исключение повторов
-                    elif value in special and op[-1] == value:
+                    elif value in special and op[-1] in special:
                         pass
                     else:
                         op += value
 
+    # Задержка
+    if delay != 0:
+        delay += 1
+        if delay > 20:
+            delay = 0
+
     # Вывод результата калькулятора
-    cv2.putText(res_img, op, (810, 165), cv2.FONT_HERSHEY_PLAIN, 3, (50, 50, 50), 3)
+    if len(op) > 12:  # Смотрим, чтобы строка не выходила за рамку калькулятора
+        cv2.putText(res_img, op[-12:], (810, 165), cv2.FONT_HERSHEY_PLAIN, 3, (50, 50, 50), 3)
+    else:
+        cv2.putText(res_img, op, (810, 165), cv2.FONT_HERSHEY_PLAIN, 3, (50, 50, 50), 3)
 
     # Задерживаем на 1 миллисекунду, ждем нажатия q
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
@@ -124,3 +154,4 @@ while cap.isOpened():
     cv2.imshow("camera", res_img)
 
 # Освобождаем ресурсы
+handsDetector.close()
